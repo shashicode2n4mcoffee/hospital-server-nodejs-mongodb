c3673b35c4f303e279bd3e08d37a2c697ce30408
shashicode2n4mcoffee <shashicode2n4mcoffee@gmail.com>
2023-05-11 21:30:24 +0530

    jwt token issue solved

--- middlewares/verifyJwtToken.js
+++ middlewares/verifyJwtToken.js
@@ -5,10 +5,10 @@ const { User } = require("../models");
 const verifyJwtToken = (req, res, next) => {
   try {
     const token = req.headers.authorization?.split(" ")[1];
-    if (!token) responseError(res, 403, {}, "Unauthorized user");
+    if (!token) return responseError(res, 403, {}, "Unauthorized user");
 
     jwt.verify(token, process.env.JWT_KEY, async (error, payload) => {
-      if (error) responseError(res, 403, {}, "Unauthorized user");
+      if (error) return responseError(res, 403, {}, "Unauthorized user");
 
       const user = await User.findById(payload.id);
       req.user = user;
